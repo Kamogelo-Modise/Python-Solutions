cc7ca1dcda0e3e180b17b26ad38bd9dd510aeda0
MohlLibPub1 <mohllibpub1@randwestcity.gov.za>
2024-07-03 11:14:57 +0200

    Added File Head Display Solution with string.txt file

--- Chapter 6 Solutons/File Head Display Solution/file_display.py
+++ Chapter 6 Solutons/File Head Display Solution/file_display.py
@@ -7,9 +7,12 @@ def main():
     
     #open file for processing
     with open(file_name,"r") as file:
+        #read line from the file
         line = file.readline().rstrip()
+        #number of line(s)
         counter = 1
         
+        #read the contents of the file
         while line != "" and counter <= 5:
             print(f"{line}")
             line = file.readline().rstrip()
